ab4292de8ca62e042ea16077e75ead35f4466ce2
Humberto Rivero <humbertorivero9@gmail.com>
2023-10-02 13:47:15 -0300

    Add inyeccion funcion en patron module

--- js/module.js
+++ js/module.js
@@ -1,19 +1,44 @@
 /**
+ * Ejemplo de un modulo
+ * 
+ */
+const printMessage = (() => {
+	return {
+		print: (message) => {
+			console.log(message);
+		},
+	};
+})();
+
+/**
+ * 
+ */
+const alertMessage = (() => {
+  return {
+    alert: (message) => {
+      alert(message);
+    },
+  };
+})();
+
+/**
  * Ejemplo del patron de diseño module
  * El modulo es una funcion que se autoejecuta y devuelve un objeto con las propiedades y metodos que queremos que sean publicos
  */
+const saludar = (function (printer) {
+	// console.log('Hola desde el modulo');
+	let mensaje = "Hola desde el modulo";
 
-const saludar = (function () {
-  // console.log('Hola desde el modulo');
-  let mensaje = 'Hola desde el modulo'
+	function concatenarTexto(nombre) {
+		return `${mensaje} ${nombre}`;
+	}
 
-  return {
-    message: function (nombre) {
-      console.log(`${mensaje} ${nombre}`);
-    }
-  }
-})()
+	return {
+		message: function (nombre) {
+			let texto = concatenarTexto(nombre);
+			printer.print(texto);
+		},
+	};
+})(printMessage);
 
-// saludar.message()
-// saludar.message('Juan')
-saludar.message('Pedro')
+saludar.message("Daniel");
